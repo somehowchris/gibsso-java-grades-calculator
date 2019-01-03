f8c40649f70198785fcf5f321fe9f9ff970c086f
Christof Weickhardt <38954618+somehowchris@users.noreply.github.com>
2019-01-03 13:31:44 +0100

    Initial commit

---
+++ src/test03/Notenrechner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test03;
+
+/**
+ *
+ * @author christoftobias.weick
+ */
+public class Notenrechner {
+    public static void main(String[] args){
+        NotenrechnerForm rechner = new NotenrechnerForm();
+        rechner.setVisible(true);
+    }
+}
